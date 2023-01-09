74607671ba51d0cfc8a879da7e6a7023dfe7cf47
Fadhil Radhian <fadhil.radhian@wartek.belajar.id>
2023-01-09 20:20:03 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Link } from "react-router-dom";
 
-function App() {
+import Routes from "./route";
+import { getTodo } from "./store/todo";
+
+export default function App() {
+  const jumlahBarang = useSelector((state) => state.keranjang.value);
+  // const todo = useSelector((state) => state.todo.list);
+
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(getTodo());
+  //   console.log(todo);
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul className="flex w-full justify-between items-center p-4 shadow-md sticky mb-4 top-0 left-0">
+            <span className="flex gap-x-6 ">
+              <li className="hover:text-blue-700 font-semibold cursor-pointer">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="hover:text-blue-700 font-semibold cursor-pointer">
+                <Link to="/keranjang">Keranjang</Link>
+              </li>
+
+              <li className="hover:text-blue-700 font-semibold cursor-pointer">
+                <Link to="/form">Form</Link>
+              </li>
+            </span>
+            <li className="text-2xl font-semibold">
+              Jumlah Item di Keranjang :{" "}
+              <span className="text-blue-600">{jumlahBarang}</span>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes />
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
